0b0f64db8f527fe2fa5cd7e43f9f70b2e40182ff
Alex <ipritoflex@yandex.ru>
2016-10-21 06:08:32 +0300

    Update

--- src/utils/telegram/telegram.js
+++ src/utils/telegram/telegram.js
@@ -31,10 +31,13 @@ async function handleInlineQuery(data) {
     id: Math.floor(Math.random() * 1e9 * 2).toString(16),
     game_short_name: 'trickyfoxy',
     name: 'Tricky Foxy',
-    reply_markup: [[{
-      text: 'Play Tricky Foxy!',
-      callback_data: 'test'
-    }]]
+    reply_markup: {
+      inline_keyboard: [[{
+        text: 'Play Tricky Foxy!',
+        callback_data: 'test',
+        callback_game: 'trickyfoxy'
+      }]]
+    }
   }];
   let results = games.filter(game => game.name.includes(query))
     .map(game => {

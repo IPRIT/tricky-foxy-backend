6da5bc9acf784bcf11286b617de68ada1b9f83a4
Alex <ipritoflex@yandex.ru>
2016-10-23 08:28:01 +0300

    Update

--- src/route/telegram/methods/getGlobalLeaderboard.js
+++ src/route/telegram/methods/getGlobalLeaderboard.js
@@ -35,7 +35,7 @@ async function handle(_data) {
     group: [ 'Highscores.userId' ],
     order: [ [ Highscore, 'score', 'DESC' ] ]
   });
-  scores = scores.map(score => score.get({plain: true})).sort((a, b) => a.score - b.score);
+  scores = scores.map(score => score.get({plain: true})).sort((a, b) => b.score - a.score);
   console.log(scores);
   return scores.slice(0, 50);
 }

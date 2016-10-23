e847cae5ff68cdf85c930e74386268b7a710df50
Alex <ipritoflex@yandex.ru>
2016-10-23 06:59:35 +0300

    Update

--- src/route/telegram/methods/expandScore.js
+++ src/route/telegram/methods/expandScore.js
@@ -47,30 +47,32 @@ function getScore(passedIslands, sessionInstance) {
     throw new HttpError();
   }
   let block = passedIslands;
+  let _prevT = 0;
   let _it = 0;
   let _iterRestrict = 1e6;
   let score = 0;
   do {
-    score = getScoreFromBlock(block, sessionInstance.createdAt);
+    score = getScoreFromBlock(block, sessionInstance.createdAt, _prevT);
+    _prevT = block._t;
     block = block._n;
     ++_it;
   } while (block && _iterRestrict > _it);
   return score;
 }
 
-function getScoreFromBlock(block, sessionCreatedAt) {
+function getScoreFromBlock(block, sessionCreatedAt, prevT) {
   let { _f, _p, _r, _s, _t } = block || {};
   
   if (!Array.isArray(_f) || _f.length !== 2) {
-    throw new HttpError('f');
+    throw new HttpError();
   } else if (_p > 1 || _p < 0) {
-    throw new HttpError('p');
+    throw new HttpError();
   } else if (_r > 1 || _r < 0) {
-    throw new HttpError('r');
-  } else if (new Date(sessionCreatedAt) > new Date(_t)) {
-    throw new HttpError('t');
+    throw new HttpError();
+  } else if (new Date(sessionCreatedAt) > new Date(_t) || _t < prevT) {
+    throw new HttpError();
   } else if (typeof _s !== 'string') {
-    throw new HttpError('s');
+    throw new HttpError();
   }
   return parseInt(_s, 20);
 }
